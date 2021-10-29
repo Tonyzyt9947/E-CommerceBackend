ffd353876f1ce47df2e280aebd696dc6795b227b
TZ <tz116917@gmail.com>
2021-10-28 21:15:50 -0700

    every "get all" tag complete

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,12 +3,30 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const Data = await Category.findAll({
+      include: [{model: Product}],
+    });
+    console.log("Route 1 running")
+    res.status(200).json(Data)
+  }catch(err){
+    res.status(500).json(err)
+  }
   // find all categories
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const category_raw = await Category.findAll({
+      include: [{model: Product}],
+    });
+    console.log("Route 1 running")
+    res.status(200).json(category_raw)
+  }catch(err){
+    res.status(500).json(err)
+  }
   // find one category by its `id` value
   // be sure to include its associated Products
 });
